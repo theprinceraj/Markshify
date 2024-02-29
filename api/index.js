ff0df8fd47032b0d4c314ff26431e430c10d989b
theprinceraj <profile.princeraj@gmail.com>
2024-02-29 08:56:36 +0530

    Basic backend setup for running static HTML

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 import express from "express";
+import fs from "fs";
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.listen(port, () =>{
-    console.log("Made by Team Dhruv!")
-})
+const webMarkup = await fs.readFile("./public/index.html", (err) => {
+  if (err) console.log(err.message, "\n", err.stack);
+});
+app.use("/", (req, res) => {
+  res.send(webMarkup);
+});
+
+app.listen(port, () => {
+  console.log("Made by Team Dhruv!");
+});
 
 export default app;
