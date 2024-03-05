1a8425b72e67c86b2bc7c7d8e6836429dd10909b
theprinceraj <profile.princeraj@gmail.com>
2024-03-05 17:16:17 +0530

    Basic script file for frontend

--- src/controllers/scanner.js
+++ src/controllers/scanner.js
@@ -2,13 +2,16 @@ import fs from "fs";
 import dotenv from "dotenv";
 dotenv.config();
 
+import { toBase64Format } from "../utilities/toBase64Format.js";
+
 export default async function ocrScanFile(inputFile) {
   try {
-    const base64String = convertToBase64(inputFile);
+    const base64String = await toBase64Format(inputFile);
     let formData = new FormData();
     formData.append("apikey", process.env.OCR_SPACE_API_KEY);
     formData.append("base64Image", base64String);
     formData.append("filetype", "png");
+    formData.append("isTable", true)
 
     const response = await fetch("https://api.ocr.space/parse/image", {
       method: "POST",
@@ -22,14 +25,10 @@ export default async function ocrScanFile(inputFile) {
     }
 
     const data = await response.json();
-    console.log(data);
+    console.log(data.ParsedResults[0].ParsedText);
   } catch (error) {
     console.log(error);
   }
 }
 const assetFile = fs.readFileSync("./public/assets/image.png");
 ocrScanFile(assetFile);
-
-function convertToBase64(file) {
-  return "data:image/png;base64," + file.toString("base64");
-}
