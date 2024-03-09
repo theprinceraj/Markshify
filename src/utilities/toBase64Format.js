523b114bdf1b052f487cf319a14b7e1840397ee4
theprinceraj <profile.princeraj@gmail.com>
2024-03-09 18:51:04 +0530

    Deleted unnecessary utility functions

--- src/utilities/toBase64Format.js
+++
@@ -1,6 +0,0 @@
-import imageType from "image-type";
-
-export async function toBase64Format(inputImage) {
-  const { ext: fileExt } = await imageType(inputImage);
-  return `data:image/${fileExt};base64,` + inputImage.toString("base64");
-}
