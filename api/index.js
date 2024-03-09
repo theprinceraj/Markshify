f435c2b01e70e4bef57cbbc1288bd62e0e780e2e
theprinceraj <profile.princeraj@gmail.com>
2024-03-09 18:36:33 +0530

    Remove unnecessary dependencies and code.

--- api/index.js
+++ api/index.js
@@ -1,26 +1,20 @@
 import express from "express";
-import fs from "fs";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(express.static("public"));
 
-const webMarkup = await fs.readFile("./public/index.html", (err) => {
-  if (err) console.log(err.message, "\n", err.stack);
-});
-app.use("/", (req, res) => {
-  res.send(webMarkup);
-});
 
 import { ocrScanFile } from "../src/controllers/scanner.js";
-app.use(express.json());
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "10mb" }));
 app.use("/scan", async (req, res) => {
+  res.json({ message: "Hello from the backend!" });
   const base64versionImage = req.body.image;
   if (!base64versionImage) {
-    return res.status(400).json({ error: "Image data is missing in the request body" });
+    return res
+      .status(400)
+      .json({ error: "Image data is missing in the request body" });
   }
 
   try {
