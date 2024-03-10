b8699bb858880b565b78d2135e61910c95b548a4
theprinceraj <profile.princeraj@gmail.com>
2024-03-10 12:36:25 +0530

    Extract relevant information from OCR lines in a different format

--- src/utilities/extractRelevantInformation.js
+++ src/utilities/extractRelevantInformation.js
@@ -1,7 +1,63 @@
+import { parse } from "dotenv";
+
 export function extractRelevantInformation(ocrLines) {
-  let currentSemester = convertRomanNumeralToInteger(ocrLines[0].LineText.split(":")[1].trim());
-  
-  console.log(currentSemester);
+  let currentSemester = convertRomanNumeralToInteger(
+    ocrLines[4].LineText?.split(":")[1].trim()
+  );
+  let registrationNo = parseInt(ocrLines[26].LineText.trim());
+  let student = ocrLines[27].LineText.trim();
+  let father = ocrLines[28].LineText.trim();
+  let mother = ocrLines[32].LineText.trim();
+  let course = ocrLines[34].LineText.trim();
+  let theoryMarksSem1_011201 = parseInt(ocrLines[76].LineText.trim());
+  let theoryMarksSem1_011202 = parseInt(ocrLines[77].LineText.trim());
+  let theoryMarksSem1_021201 = parseInt(ocrLines[78].LineText.trim());
+  let theoryMarksSem1_211202 = parseInt(ocrLines[79].LineText.trim());
+  let theoryMarksSem1_231201 = parseInt(ocrLines[80].LineText.trim());
+  console.log(
+    formatOutput(
+      registrationNo,
+      student,
+      father,
+      mother,
+      currentSemester,
+      course,
+      theoryMarksSem1_011201,
+      theoryMarksSem1_011202,
+      theoryMarksSem1_021201,
+      theoryMarksSem1_211202,
+      theoryMarksSem1_231201
+    )
+  );
+}
+
+function formatOutput(
+  registrationNo,
+  student,
+  father,
+  mother,
+  currentSemester,
+  course,
+  theoryMarksSem1_011201,
+  theoryMarksSem1_011202,
+  theoryMarksSem1_021201,
+  theoryMarksSem1_211202,
+  theoryMarksSem1_231201
+) {
+  return (
+    "Registration Number,Name,Father,Mother,Course,Semester,Theory Marks\n"+
+    `${registrationNo}, ` +
+      `${student}, ` +
+      `${father}, ` +
+      `${mother}, ` +
+      `${currentSemester}, ` +
+      `${course}, ` +
+      `${theoryMarksSem1_011201}, ` +
+      `${theoryMarksSem1_011202}, ` +
+      `${theoryMarksSem1_021201}, ` +
+      `${theoryMarksSem1_211202}, ` +
+      `${theoryMarksSem1_231201}`
+  );
 }
 
 function convertRomanNumeralToInteger(semesterInRoman) {
