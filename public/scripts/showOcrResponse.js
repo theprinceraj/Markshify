297cf30eae94291e25a3592bdae9d8ddfe91045b
theprinceraj <profile.princeraj@gmail.com>
2024-03-11 13:18:52 +0530

    Feat: Shows alert dialog box when falsy value received as ocr response

--- public/scripts/showOcrResponse.js
+++ public/scripts/showOcrResponse.js
@@ -4,7 +4,13 @@
  * @param {string} responseString - The OCR response to be displayed
  * @return {void}
  */
-function displayOcrResponse(responseString){
-    const outputDiv = document.getElementById("output");
-    outputDiv.innerText = responseString;
+function displayOcrResponse(responseString) {
+  if (!responseString) {
+    alert(
+      "An error encountered. Please recheck that you have scanned the correct file and that it is absolutely clear."
+    );
+    return;
+  }
+  const outputDiv = document.getElementById("output");
+  outputDiv.innerText = responseString;
 }
