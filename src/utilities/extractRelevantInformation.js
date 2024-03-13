437e6ee258350f66c892fd7c7b2c09dead5c3530
theprinceraj <profile.princeraj@gmail.com>
2024-03-13 18:08:23 +0530

    .

--- src/utilities/extractRelevantInformation.js
+++ src/utilities/extractRelevantInformation.js
@@ -1,34 +1,37 @@
 export function extractRelevantInformation(ocrLines) {
+  console.log(ocrLines)
   let currentSemester = convertRomanNumeralToInteger(
-    ocrLines[4].LineText?.split(":")[1].trim()
+    ocrLines[4]?.LineText?.split(":")[1].trim()
   );
-  let registrationNo = parseInt(ocrLines[26].LineText.trim());
-  let student = ocrLines[27].LineText.trim();
-  let father = ocrLines[28].LineText.trim();
-  let mother = ocrLines[32].LineText.trim();
-  let course = ocrLines[34].LineText.trim();
+  let registrationNo = parseInt(ocrLines[26]?.LineText.trim());
+  let student = ocrLines[27]?.LineText.trim();
+  let father = ocrLines[28]?.LineText.trim();
+  let mother = ocrLines[32]?.LineText.trim();
+  let course = ocrLines[34]?.LineText.trim();
   ocrLines.splice(0, 76);
-  let theoryMarksSem1_011201 = parseInt(ocrLines[0].LineText.trim());
-  let theoryMarksSem1_011202 = parseInt(ocrLines[1].LineText.trim());
-  let theoryMarksSem1_021201 = parseInt(ocrLines[2].LineText.trim());
-  let theoryMarksSem1_211202 = parseInt(ocrLines[3].LineText.trim());
-  let theoryMarksSem1_231201 = parseInt(ocrLines[4].LineText.trim());
-  let practicalMarksSem1_011201 = parseInt(ocrLines[6].LineText.trim());
-  let practicalMarksSem1_011202 = parseInt(ocrLines[7].LineText.trim());
-  let practicalMarksSem1_021201 = parseInt(ocrLines[8].LineText.trim());
-  let practicalMarksSem1_021203 = parseInt(ocrLines[9].LineText.trim());
-  let practicalMarksSem1_231201 = parseInt(ocrLines[10].LineText.trim());
-    
+  ocrLines.splice(ocrLines.length - 8, 8);
+  let theoryMarksSem1_011201 = parseInt(ocrLines[0]?.LineText.trim());
+  let theoryMarksSem1_011202 = parseInt(ocrLines[1]?.LineText.trim());
+  let theoryMarksSem1_021201 = parseInt(ocrLines[2]?.LineText.trim());
+  let theoryMarksSem1_211202 = parseInt(ocrLines[3]?.LineText.trim());
+  let theoryMarksSem1_231201 = parseInt(ocrLines[4]?.LineText.trim());
+  let practicalMarksSem1_011201 = parseInt(ocrLines[6]?.LineText.trim());
+  let practicalMarksSem1_011202 = parseInt(ocrLines[7]?.LineText.trim());
+  let practicalMarksSem1_021201 = parseInt(ocrLines[8]?.LineText.trim());
+  let practicalMarksSem1_021203 = parseInt(ocrLines[9]?.LineText.trim());
+  let practicalMarksSem1_231201 = parseInt(ocrLines[10]?.LineText.trim());
+  ocrLines.splice(0, 27);
   // console.log(ocrLines);
-  // let semGPAs = new Array(8).fill(NaN);
-  // semGPAs[0] = parseFloat(ocrLines[103].LineText.trim());
-  // semGPAs[1] = parseFloat(ocrLines[105].LineText.trim()) || NaN;
-  // semGPAs[2] = parseFloat(ocrLines[107].LineText.trim()) || NaN;
-  // semGPAs[3] = parseFloat(ocrLines[110].LineText.trim()) || NaN;
-  // semGPAs[4] = parseFloat(ocrLines[112].LineText.trim()) || NaN;
-  // semGPAs[5] = parseFloat(ocrLines[114].LineText.trim()) || NaN;
-  // semGPAs[6] = parseFloat(ocrLines[116].LineText.trim()) || NaN;
-  // semGPAs[7] = parseFloat(ocrLines[118].LineText.trim()) || NaN;
+  let semGPAs = new Array(8).fill(NaN);
+  semGPAs[0] = parseFloat(ocrLines[0]?.LineText.trim()) || NaN;
+  semGPAs[1] = parseFloat(ocrLines[2]?.LineText.trim()) || NaN;
+  semGPAs[2] = parseFloat(ocrLines[4]?.LineText.trim()) || NaN;
+  // Incorrectly takes in Curr. GPA in 4th element of array
+  semGPAs[3] = parseFloat(ocrLines[6]?.LineText.trim()) || NaN;
+  semGPAs[4] = parseFloat(ocrLines[8]?.LineText.trim()) || NaN;
+  semGPAs[5] = parseFloat(ocrLines[10]?.LineText.trim()) || NaN;
+  semGPAs[6] = parseFloat(ocrLines[12]?.LineText.trim()) || NaN;
+  semGPAs[7] = parseFloat(ocrLines[14]?.LineText.trim()) || NaN;
   // console.log(semGPAs);
   return formatOutput(
     registrationNo,
