4e0d142588502c6b1e7a2c78bc08c0275455b304
theprinceraj <profile.princeraj@gmail.com>
2024-03-13 16:43:15 +0530

    Speed Improvements

--- src/utilities/extractRelevantInformation.js
+++ src/utilities/extractRelevantInformation.js
@@ -7,16 +7,29 @@ export function extractRelevantInformation(ocrLines) {
   let father = ocrLines[28].LineText.trim();
   let mother = ocrLines[32].LineText.trim();
   let course = ocrLines[34].LineText.trim();
-  let theoryMarksSem1_011201 = parseInt(ocrLines[76].LineText.trim());
-  let theoryMarksSem1_011202 = parseInt(ocrLines[77].LineText.trim());
-  let theoryMarksSem1_021201 = parseInt(ocrLines[78].LineText.trim());
-  let theoryMarksSem1_211202 = parseInt(ocrLines[79].LineText.trim());
-  let theoryMarksSem1_231201 = parseInt(ocrLines[80].LineText.trim());
-  let practicalMarksSem1_011201 = parseInt(ocrLines[82].LineText.trim());
-  let practicalMarksSem1_011202 = parseInt(ocrLines[83].LineText.trim());
-  let practicalMarksSem1_021201 = parseInt(ocrLines[84].LineText.trim());
-  let practicalMarksSem1_021203 = parseInt(ocrLines[85].LineText.trim());
-  let practicalMarksSem1_231201 = parseInt(ocrLines[86].LineText.trim());
+  ocrLines.splice(0, 76);
+  let theoryMarksSem1_011201 = parseInt(ocrLines[0].LineText.trim());
+  let theoryMarksSem1_011202 = parseInt(ocrLines[1].LineText.trim());
+  let theoryMarksSem1_021201 = parseInt(ocrLines[2].LineText.trim());
+  let theoryMarksSem1_211202 = parseInt(ocrLines[3].LineText.trim());
+  let theoryMarksSem1_231201 = parseInt(ocrLines[4].LineText.trim());
+  let practicalMarksSem1_011201 = parseInt(ocrLines[6].LineText.trim());
+  let practicalMarksSem1_011202 = parseInt(ocrLines[7].LineText.trim());
+  let practicalMarksSem1_021201 = parseInt(ocrLines[8].LineText.trim());
+  let practicalMarksSem1_021203 = parseInt(ocrLines[9].LineText.trim());
+  let practicalMarksSem1_231201 = parseInt(ocrLines[10].LineText.trim());
+    
+  // console.log(ocrLines);
+  // let semGPAs = new Array(8).fill(NaN);
+  // semGPAs[0] = parseFloat(ocrLines[103].LineText.trim());
+  // semGPAs[1] = parseFloat(ocrLines[105].LineText.trim()) || NaN;
+  // semGPAs[2] = parseFloat(ocrLines[107].LineText.trim()) || NaN;
+  // semGPAs[3] = parseFloat(ocrLines[110].LineText.trim()) || NaN;
+  // semGPAs[4] = parseFloat(ocrLines[112].LineText.trim()) || NaN;
+  // semGPAs[5] = parseFloat(ocrLines[114].LineText.trim()) || NaN;
+  // semGPAs[6] = parseFloat(ocrLines[116].LineText.trim()) || NaN;
+  // semGPAs[7] = parseFloat(ocrLines[118].LineText.trim()) || NaN;
+  // console.log(semGPAs);
   return formatOutput(
     registrationNo,
     student,
@@ -65,7 +78,7 @@ function formatOutput(
     `${course}, ` +
     `"` +
     `011201 - ${theoryMarksSem1_011201}, ` +
-    `011201 - ${theoryMarksSem1_011202}, ` +
+    `011202 - ${theoryMarksSem1_011202}, ` +
     `021201 - ${theoryMarksSem1_021201}, ` +
     `211202 - ${theoryMarksSem1_211202}, ` +
     `231201 - ${theoryMarksSem1_231201}` +
