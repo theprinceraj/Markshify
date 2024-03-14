91c9b9c640e35f3bf280b3d16d5bc617b289a5d9
AD11-web <ankithdas065@gmail.com>
2024-03-14 15:46:47 +0530

    .

--- src/utilities/preProcessImage.js
+++ src/utilities/preProcessImage.js
@@ -1,17 +1,11 @@
 import sharp from "sharp";
 import fs from "fs";
-let imageFile;
-fs.readFileSync("./image.png").then((img) => {
-  imageFile = img;
-});
+let imageFile = fs.readFileSync("src/utilities/image.png")
 
 async function preProcessImage(InputImage) {
-  sharp(InputImage)
-    .extract({
-      left: 167,
-      top: 674,
-      width: 2134,
-      height: 1967,
-    })
-    .toFile("output.jpg", function (err) {});
+    // Write code below this line
+
+
+    // Write code above this line
 }
+preProcessImage(imageFile)
