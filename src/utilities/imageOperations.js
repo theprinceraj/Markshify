6f9a324a473c8ba41a1a6e14582f0133ca7eda41
AD11-web <ankithdas065@gmail.com>
2024-03-20 14:33:54 +0530

    done

--- src/utilities/imageOperations.js
+++
@@ -1,6 +0,0 @@
-const sharp = require("sharp")
-
-sharp("image.png")
-.resize({ width: 2181, height: 2164 })
-.greyscale()
-.toFile("outputImage1.png");
--- src/utilities/preProcessImage.js
+++ src/utilities/preProcessImage.js
@@ -1,20 +1,11 @@
-// import sharp from "sharp";
-// import fs from "fs";
-// let imageFile = fs.readFileSync("src/utilities/image.png")
+import sharp from "sharp";
 
-// async function preProcessImage(InputImage) {
-//     // Write code below this line
-//     try {
-//         // Convert the input image to grayscale
-//         await sharp(inputImage)
-//             .greyscale() // Convert to grayscale
-//             .toFile('outputGrayscaleImage.png'); 
+export async function preProcessImage(imageBuffer) {
+  const response = await sharp(imageBuffer)
+    .extract({ width: 2113, height: 1807, top: 805, left: 181 })
+    .greyscale()
+    .toFile("outputImage2.png")
+    .toBuffer();
 
-//         console.log('Grayscale conversion completed successfully.');
-//     } catch (error) {
-//         console.error('Error processing the image:', error);
-//         throw error;
-//     }
-// }
-//     // Write code above this line
-// preProcessImage(imageFile)
+  return response;
+}
