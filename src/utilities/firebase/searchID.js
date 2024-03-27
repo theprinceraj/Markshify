69cf0b130f82d1234ffc6b4b5be12b3cac4bfb35
theprinceraj <profile.princeraj@gmail.com>
2024-03-27 16:11:28 +0530

    .

---
+++ src/utilities/firebase/searchID.js
@@ -0,0 +1,8 @@
+import { getDatabase, ref, set } from "firebase/database";
+import app from "./firebaseInitialize.js";
+
+const db = getDatabase(app);
+
+export async function searchID(regNo) {
+    
+}
