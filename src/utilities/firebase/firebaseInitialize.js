55e796990bb463b43d7618d1f9f8fc5bd60468b5
theprinceraj <profile.princeraj@gmail.com>
2024-04-02 20:31:19 +0530

    .

--- src/utilities/firebase/firebaseInitialize.js
+++ src/utilities/firebase/firebaseInitialize.js
@@ -1,12 +1,25 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+// };
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyC_5Arobi0BdImMSBBKree9XKks-pqtkxs",
+  authDomain: "smart-sheet-generator.firebaseapp.com",
+  databaseURL: "https://smart-sheet-generator-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "smart-sheet-generator",
+  storageBucket: "smart-sheet-generator.appspot.com",
+  messagingSenderId: "851537036727",
+  appId: "1:851537036727:web:219e53c0ff4b3d67d25432"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
---
+++ src/utilities/firebase/uploadStudentData.js
@@ -0,0 +1,52 @@
+import { db } from "./firebaseInitialize.js";
+import { collection, addDoc } from "@firebase/firestore";
+
+export async function uploadStudentData({
+  regNo,
+  studentName,
+  fatherName,
+  motherName,
+  courseName,
+  semester,
+  subCode,
+  subName,
+  totalMarks,
+  isPractical,
+  sgpa,
+  cgpa,
+}) {
+  try {
+    const studentRef = collection(db, "students");
+    await addDoc(studentRef, {
+      registrationNumber: regNo,
+      name: studentName,
+      fatherName: fatherName,
+      motherName: motherName,
+      courseName: courseName,
+      semester: semester,
+      subCode: subCode,
+      subName: subName,
+      totalMarks: totalMarks,
+      isPractical: isPractical,
+      sgpa: sgpa,
+      cgpa: cgpa,
+    });
+  } catch (err) {
+    console.log("Error occured inside uploadStudentData function:\n", err);
+  }
+}
+
+await uploadStudentData({
+  regNo: 17102110031,
+  studentName: "Student1",
+  fatherName: "Father1",
+  motherName: "Mother1",
+  courseName: "B.Tech.",
+  semester: 1,
+  subCode: "SUB1",
+  subName: "Subject1",
+  totalMarks: 100,
+  isPractical: true,
+  sgpa: 9.0,
+  cgpa: 9.0,
+});
