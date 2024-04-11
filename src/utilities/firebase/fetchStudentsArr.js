2e3f9da75a25b66cc796e07a11c1af58f45fc46f
theprinceraj <profile.princeraj@gmail.com>
2024-04-11 20:42:42 +0530

    .

--- src/utilities/firebase/fetchStudentsArr.js
+++ src/utilities/firebase/fetchStudentsArr.js
@@ -4,11 +4,8 @@ export async function fetchStudentsArr() {
   try {
     const studentRef = collection(db, "students");
     const studentSnapshot = await getDocs(studentRef);
-    // console.log(studentSnapshot.docs[1].data());
     return studentSnapshot.docs.map((doc) => doc.data());
   } catch (e) {
     console.log("Error occured inside fetchStudentsArr function:\n", e);
   }
 }
-
-// await fetchStudentsArr();
